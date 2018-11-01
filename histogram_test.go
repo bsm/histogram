e219932777c945c7a221ce0e7ace7712c59c8bac
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-11-01 12:42:11 +0000

    Improved test

--- histogram_test.go
+++ histogram_test.go
@@ -12,21 +12,25 @@ import (
 )
 
 var _ = Describe("Histogram", func() {
-	blnk := seed()
-	std := seed(39, 15, 43, 7, 43, 36, 47, 6, 40, 49, 41)
+	var blank, subject *Histogram
+
+	BeforeEach(func() {
+		blank = seedHist()
+		subject = seedHist(39, 15, 43, 7, 43, 36, 47, 6, 40, 49, 41)
+	})
 
 	DescribeTable("Quantile",
-		func(h *Histogram, q float64, x float64) {
-			Expect(h.Quantile(q)).To(BeNumerically("~", x, 0.1))
+		func(q float64, x float64) {
+			Expect(subject.Quantile(q)).To(BeNumerically("~", x, 0.1))
 		},
 
-		Entry("0%", std, 0.0, 6.0),
-		Entry("25%", std, 0.25, 19.6),
-		Entry("50%", std, 0.5, 39.8),
-		Entry("75%", std, 0.75, 44.3),
-		Entry("95%", std, 0.95, 47.2),
-		Entry("99%", std, 0.99, 48.2),
-		Entry("100%", std, 1.0, 49.0),
+		Entry("0%", 0.0, 6.0),
+		Entry("25%", 0.25, 19.6),
+		Entry("50%", 0.5, 39.8),
+		Entry("75%", 0.75, 44.3),
+		Entry("95%", 0.95, 47.2),
+		Entry("99%", 0.99, 48.2),
+		Entry("100%", 1.0, 49.0),
 	)
 
 	// inspired by https://github.com/aaw/histosketch/commit/d8284aa#diff-11101c92fbb1d58ccf30ca49764bf202R180
@@ -60,67 +64,76 @@ var _ = Describe("Histogram", func() {
 	})
 
 	It("should reject bad quantile inputs", func() {
-		Expect(math.IsNaN(blnk.Quantile(0.5))).To(BeTrue())
-		Expect(math.IsNaN(std.Quantile(-0.1))).To(BeTrue())
-		Expect(math.IsNaN(std.Quantile(1.1))).To(BeTrue())
+		Expect(math.IsNaN(blank.Quantile(0.5))).To(BeTrue())
+		Expect(math.IsNaN(subject.Quantile(-0.1))).To(BeTrue())
+		Expect(math.IsNaN(subject.Quantile(1.1))).To(BeTrue())
 	})
 
 	It("should calc sum", func() {
-		Expect(math.IsNaN(blnk.Sum())).To(BeTrue())
-		Expect(std.Sum()).To(Equal(366.0))
+		Expect(math.IsNaN(blank.Sum())).To(BeTrue())
+		Expect(subject.Sum()).To(Equal(366.0))
 	})
 
 	It("should calc mean", func() {
-		Expect(math.IsNaN(blnk.Mean())).To(BeTrue())
-		Expect(std.Mean()).To(BeNumerically("~", 33.27, 0.01))
+		Expect(math.IsNaN(blank.Mean())).To(BeTrue())
+		Expect(subject.Mean()).To(BeNumerically("~", 33.27, 0.01))
 	})
 
 	It("should calc count", func() {
-		Expect(blnk.Count()).To(Equal(0))
-		Expect(std.Count()).To(Equal(11))
+		Expect(blank.Count()).To(Equal(0))
+		Expect(subject.Count()).To(Equal(11))
 	})
 
 	It("should calc weight", func() {
-		Expect(blnk.Weight()).To(Equal(0.0))
-		Expect(std.Weight()).To(Equal(11.0))
+		Expect(blank.Weight()).To(Equal(0.0))
+		Expect(subject.Weight()).To(Equal(11.0))
 	})
 
 	It("should calc min", func() {
-		Expect(math.IsNaN(blnk.Min())).To(BeTrue())
-		Expect(std.Min()).To(Equal(float64(6)))
+		Expect(math.IsNaN(blank.Min())).To(BeTrue())
+		Expect(subject.Min()).To(Equal(float64(6)))
 	})
 
 	It("should copy", func() {
-		c1 := std.Copy(nil)
-		Expect(c1).To(Equal(std))
+		c1 := subject.Copy(nil)
+		Expect(c1).To(Equal(subject))
 
-		t2 := seed(1, 2, 3, 4)
-		c2 := std.Copy(t2)
-		Expect(c2).To(Equal(std))
+		t2 := seedHist(1, 2, 3, 4)
+		c2 := subject.Copy(t2)
+		Expect(c2).To(Equal(subject))
 		Expect(c2).To(Equal(t2))
 	})
 
 	It("should calc max", func() {
-		Expect(math.IsNaN(blnk.Max())).To(BeTrue())
-		Expect(std.Max()).To(Equal(float64(49)))
+		Expect(math.IsNaN(blank.Max())).To(BeTrue())
+		Expect(subject.Max()).To(Equal(float64(49)))
 	})
 
 	It("should merge", func() {
-		h2 := seed(11, 2, 3, 14, 7, 4)
+		h2 := seedHist(11, 2, 3, 14, 7, 4)
 		Expect(h2.Sum()).To(Equal(41.0))
 		Expect(h2.bins).To(HaveLen(4))
 
-		h2.MergeWith(std)
+		h2.MergeWith(subject)
 		Expect(h2.Sum()).To(Equal(407.0))
 		Expect(h2.bins).To(HaveLen(4))
 	})
 
-	It("should add", func() {
-		Expect(std.bins).To(HaveLen(4))
-		Expect(std.bins).To(HaveCap(5))
-		Expect(std.bins).To(Equal([]bin{
+	It("should add with weight", func() {
+		Expect(subject.bins).To(HaveLen(4))
+		Expect(subject.bins).To(HaveCap(5))
+		Expect(subject.bins).To(Equal([]bin{
 			{w: -2, v: 6.5},
 			{w: 1, v: 15},
+			{w: -4, v: 39},
+			{w: -4, v: 45.5},
+		}))
+
+		subject.AddWeight(6.5, 2.0)
+		subject.AddWeight(15, 3.0)
+		Expect(subject.bins).To(Equal([]bin{
+			{w: -4, v: 6.5},
+			{w: 4, v: 15},
 			{w: -4, v: 39},
 			{w: -4, v: 45.5},
 		}))
@@ -135,7 +148,7 @@ func TestSuite(t *testing.T) {
 	RunSpecs(t, "histogram")
 }
 
-func seed(vv ...float64) *Histogram {
+func seedHist(vv ...float64) *Histogram {
 	h := New(4)
 	for _, v := range vv {
 		h.Add(v)
