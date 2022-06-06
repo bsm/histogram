13d8e7a30ec43ea1085d46deb5d6c5f2a6cc1aad
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2022-06-06 10:53:08 +0100

    Reduce deps

--- histogram_test.go
+++ histogram_test.go
@@ -1,183 +1,14 @@
-package histogram
+package histogram_test
 
 import (
 	"math"
 	"math/rand"
+	"reflect"
 	"sort"
 	"testing"
 
-	. "github.com/bsm/ginkgo"
-	. "github.com/bsm/ginkgo/extensions/table"
-	. "github.com/bsm/gomega"
+	. "github.com/bsm/histogram/v3"
 )
-
-var _ = Describe("Histogram", func() {
-	var blank, subject *Histogram
-
-	BeforeEach(func() {
-		blank = seedHist()
-		subject = seedHist(39, 15, 43, 7, 43, 36, 47, 6, 40, 49, 41)
-	})
-
-	DescribeTable("Quantile",
-		func(q float64, x float64) {
-			Expect(subject.Quantile(q)).To(BeNumerically("~", x, 0.1))
-		},
-
-		Entry("0%", 0.0, 6.0),
-		Entry("25%", 0.25, 19.6),
-		Entry("50%", 0.5, 39.8),
-		Entry("75%", 0.75, 44.3),
-		Entry("95%", 0.95, 47.2),
-		Entry("99%", 0.99, 48.2),
-		Entry("100%", 1.0, 49.0),
-	)
-
-	// inspired by https://github.com/aaw/histosketch/commit/d8284aa#diff-11101c92fbb1d58ccf30ca49764bf202R180
-	// released into the public domain
-	It("should accurately predict quantile", func() {
-		N := 20_000
-		Q1 := []float64{0.001, 0.01, 0.1, 0.25, 0.35, 0.65, 0.75, 0.9, 0.99, 0.999}
-		Q2 := []float64{0.0001, 0.9999}
-
-		for seed := int64(0); seed < 16; seed++ {
-			r := rand.New(rand.NewSource(seed))
-			h := New(100)           // histogram
-			x := make([]float64, N) // exact
-
-			for i := 0; i < N; i++ {
-				num := r.NormFloat64() * 1
-				h.Add(num)
-				x[i] = num
-			}
-			sort.Float64s(x)
-
-			for _, q := range Q1 {
-				tQ := h.Quantile(q)
-				xQ := x[int(float64(len(x))*q)]
-				re := math.Abs((tQ - xQ) / xQ)
-
-				Expect(re).To(BeNumerically("<", 0.02), // allow ±2%
-					"s.Quantile(%v) (got %.3f, want %.3f with seed = %v)", q, tQ, xQ, seed,
-				)
-			}
-
-			for _, q := range Q2 {
-				tQ := h.Quantile(q)
-				xQ := x[int(float64(len(x))*q)]
-				re := math.Abs((tQ - xQ) / xQ)
-
-				Expect(re).To(BeNumerically("<", 0.1), // allow ±10%
-					"s.Quantile(%v) (got %.3f, want %.3f with seed = %v)", q, tQ, xQ, seed,
-				)
-			}
-		}
-	})
-
-	It("should reject bad quantile inputs", func() {
-		Expect(math.IsNaN(blank.Quantile(0.5))).To(BeTrue())
-		Expect(math.IsNaN(subject.Quantile(-0.1))).To(BeTrue())
-		Expect(math.IsNaN(subject.Quantile(1.1))).To(BeTrue())
-	})
-
-	It("should calc sum", func() {
-		Expect(math.IsNaN(blank.Sum())).To(BeTrue())
-		Expect(subject.Sum()).To(Equal(366.0))
-	})
-
-	It("should calc mean", func() {
-		Expect(math.IsNaN(blank.Mean())).To(BeTrue())
-		Expect(subject.Mean()).To(BeNumerically("~", 33.27, 0.01))
-	})
-
-	It("should calc count", func() {
-		Expect(blank.Count()).To(Equal(0))
-		Expect(subject.Count()).To(Equal(11))
-	})
-
-	It("should calc weight", func() {
-		Expect(blank.Weight()).To(Equal(0.0))
-		Expect(subject.Weight()).To(Equal(11.0))
-	})
-
-	It("should calc min", func() {
-		Expect(math.IsNaN(blank.Min())).To(BeTrue())
-		Expect(subject.Min()).To(Equal(float64(6)))
-	})
-
-	It("should copy", func() {
-		c1 := subject.Copy(nil)
-		Expect(c1).To(Equal(subject))
-
-		t2 := seedHist(1, 2, 3, 4)
-		c2 := subject.Copy(t2)
-		Expect(c2).To(Equal(subject))
-		Expect(c2).To(Equal(t2))
-	})
-
-	It("should calc max", func() {
-		Expect(math.IsNaN(blank.Max())).To(BeTrue())
-		Expect(subject.Max()).To(Equal(float64(49)))
-	})
-
-	It("should merge", func() {
-		h2 := seedHist(11, 2, 3, 14, 7, 4)
-		Expect(h2.Sum()).To(Equal(41.0))
-		Expect(h2.bins).To(HaveLen(4))
-
-		h2.MergeWith(subject)
-		Expect(h2.Sum()).To(Equal(407.0))
-		Expect(h2.bins).To(HaveLen(4))
-	})
-
-	It("should add with weight", func() {
-		Expect(subject.bins).To(HaveLen(4))
-		Expect(subject.bins).To(HaveCap(5))
-		Expect(subject.bins).To(Equal([]bin{
-			{w: -2, v: 6.5},
-			{w: 1, v: 15},
-			{w: -4, v: 39},
-			{w: -4, v: 45.5},
-		}))
-
-		subject.AddWeight(6.5, 2.0)
-		subject.AddWeight(15, 3.0)
-		Expect(subject.bins).To(Equal([]bin{
-			{w: -4, v: 6.5},
-			{w: 4, v: 15},
-			{w: -4, v: 39},
-			{w: -4, v: 45.5},
-		}))
-	})
-
-	It("should return bin count and data", func() {
-		Expect(subject.NumBins()).To(Equal(4))
-
-		v, w := subject.Bin(0)
-		Expect(v).To(BeNumerically("==", 6.5))
-		Expect(w).To(BeNumerically("==", -2))
-
-		v, w = subject.Bin(1)
-		Expect(v).To(BeNumerically("==", 15))
-		Expect(w).To(BeNumerically("==", 1))
-
-		v, w = subject.Bin(2)
-		Expect(v).To(BeNumerically("==", 39))
-		Expect(w).To(BeNumerically("==", -4))
-
-		v, w = subject.Bin(3)
-		Expect(v).To(BeNumerically("==", 45.5))
-		Expect(w).To(BeNumerically("==", -4))
-	})
-
-})
-
-// --------------------------------------------------------------------
-
-func TestSuite(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "histogram")
-}
 
 func seedHist(vv ...float64) *Histogram {
 	h := New(4)
@@ -185,4 +16,207 @@ func seedHist(vv ...float64) *Histogram {
 		h.Add(v)
 	}
 	return h
+}
+
+var (
+	blank = seedHist()
+	hist  = seedHist(39, 15, 43, 7, 43, 36, 47, 6, 40, 49, 41)
+)
+
+func expectQ(t *testing.T, q, exp float64) {
+	t.Helper()
+
+	if got := hist.Quantile(q); math.Abs(exp-got) > 0.1 {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Quantile(t *testing.T) {
+	t.Run("0%", func(t *testing.T) { expectQ(t, 0.0, 6.0) })
+	t.Run("25%", func(t *testing.T) { expectQ(t, 0.25, 19.6) })
+	t.Run("50%", func(t *testing.T) { expectQ(t, 0.5, 39.8) })
+	t.Run("75%", func(t *testing.T) { expectQ(t, 0.75, 44.3) })
+	t.Run("95%", func(t *testing.T) { expectQ(t, 0.95, 47.2) })
+	t.Run("99%", func(t *testing.T) { expectQ(t, 0.99, 48.2) })
+	t.Run("100%", func(t *testing.T) { expectQ(t, 1.0, 49.0) })
+}
+
+// inspired by https://github.com/aaw/histosketch/commit/d8284aa#diff-11101c92fbb1d58ccf30ca49764bf202R180
+// released into the public domain
+func TestHistogram_Quantile_accuracy(t *testing.T) {
+	N := 20_000
+	Q1 := []float64{0.001, 0.01, 0.1, 0.25, 0.35, 0.65, 0.75, 0.9, 0.99, 0.999}
+	Q2 := []float64{0.0001, 0.9999}
+
+	for seed := int64(0); seed < 16; seed++ {
+		r := rand.New(rand.NewSource(seed))
+		h := New(100)           // histogram
+		x := make([]float64, N) // exact
+
+		for i := 0; i < N; i++ {
+			num := r.NormFloat64() * 1
+			h.Add(num)
+			x[i] = num
+		}
+		sort.Float64s(x)
+
+		for _, q := range Q1 {
+			tQ := h.Quantile(q)
+			xQ := x[int(float64(len(x))*q)]
+
+			// allow ±2%
+			if re := math.Abs((tQ - xQ) / xQ); re > 0.02 {
+				t.Errorf("s.Quantile(%v) (got %.3f, want %.3f with seed = %v)", q, tQ, xQ, seed)
+			}
+		}
+
+		for _, q := range Q2 {
+			tQ := h.Quantile(q)
+			xQ := x[int(float64(len(x))*q)]
+
+			// allow ±10%
+			if re := math.Abs((tQ - xQ) / xQ); re > 0.1 {
+				t.Errorf("s.Quantile(%v) (got %.3f, want %.3f with seed = %v)", q, tQ, xQ, seed)
+			}
+		}
+	}
+}
+
+func TestHistogram_Quantile_nan(t *testing.T) {
+	if !math.IsNaN(blank.Quantile(0.5)) {
+		t.Errorf("expected IsNaN to be true")
+	}
+	if !math.IsNaN(hist.Quantile(-0.1)) {
+		t.Errorf("expected IsNaN to be true")
+	}
+	if !math.IsNaN(hist.Quantile(1.1)) {
+		t.Errorf("expected IsNaN to be true")
+	}
+}
+
+func TestHistogram_Sum(t *testing.T) {
+	if !math.IsNaN(blank.Sum()) {
+		t.Errorf("expected IsNaN to be true")
+	}
+
+	if exp, got := 366.0, hist.Sum(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+func TestHistogram_Mean(t *testing.T) {
+	if !math.IsNaN(blank.Mean()) {
+		t.Errorf("expected IsNaN to be true")
+	}
+
+	if exp, got := 33.27272727272727, hist.Mean(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Count(t *testing.T) {
+	if exp, got := 0, blank.Count(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+
+	if exp, got := 11, hist.Count(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Weight(t *testing.T) {
+	if exp, got := 0.0, blank.Weight(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+
+	if exp, got := 11.0, hist.Weight(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Min(t *testing.T) {
+	if !math.IsNaN(blank.Min()) {
+		t.Errorf("expected IsNaN to be true")
+	}
+
+	if exp, got := 6.0, hist.Min(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Max(t *testing.T) {
+	if !math.IsNaN(blank.Max()) {
+		t.Errorf("expected IsNaN to be true")
+	}
+
+	if exp, got := 49.0, hist.Max(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Copy(t *testing.T) {
+	dupe := hist.Copy(nil)
+	if !reflect.DeepEqual(hist, dupe) {
+		t.Fatalf("expected %v, got %v", hist, dupe)
+	}
+
+	dupe.Add(34)
+	if exp, got := 12, dupe.Count(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+	if exp, got := 11, hist.Count(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Merge(t *testing.T) {
+	his2 := seedHist(11, 2, 3, 14, 7, 4)
+	if exp, got := 41.0, his2.Sum(); exp != got {
+		t.Fatalf("expected %v, got %v", exp, got)
+	}
+
+	his2.MergeWith(hist)
+	if exp, got := 407.0, his2.Sum(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+	if exp, got := 366.0, hist.Sum(); exp != got {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestHistogram_Bin(t *testing.T) {
+	if exp, got := 4, hist.NumBins(); exp != got {
+		t.Fatalf("expected %v, got %v", exp, got)
+	}
+
+	v, w := hist.Bin(0)
+	if exp := 6.5; exp != v {
+		t.Errorf("expected %v, got %v", exp, v)
+	}
+	if exp := -2.0; exp != w {
+		t.Errorf("expected %v, got %v", exp, w)
+	}
+
+	v, w = hist.Bin(1)
+	if exp := 15.0; exp != v {
+		t.Errorf("expected %v, got %v", exp, v)
+	}
+	if exp := 1.0; exp != w {
+		t.Errorf("expected %v, got %v", exp, w)
+	}
+
+	v, w = hist.Bin(2)
+	if exp := 39.0; exp != v {
+		t.Errorf("expected %v, got %v", exp, v)
+	}
+	if exp := -4.0; exp != w {
+		t.Errorf("expected %v, got %v", exp, w)
+	}
+
+	v, w = hist.Bin(3)
+	if exp := 45.5; exp != v {
+		t.Errorf("expected %v, got %v", exp, v)
+	}
+	if exp := -4.0; exp != w {
+		t.Errorf("expected %v, got %v", exp, w)
+	}
 }
